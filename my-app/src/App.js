79f7f5c7e025e7123ba751fda91dabf67d760d6d
JessFonseca <jfwolverine93@gmail.com>
2018-09-23 17:14:24 -0700

    sjldfj

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,42 @@
-import React, { Component } from 'react';
-import ColorPickerInput from './ColorPickerInput';
-import './App.css';
-import ColorPickerList from './ColorPickerList';
+import React, { Component } from "react";
+import ColorPickerInput from "./ColorPickerInput";
+import "./App.css";
+import ColorPickerList from "./ColorPickerList";
 
 class App extends Component {
   state = {
-    bgColor: "white",
-  }
+    bgColor: "white"
+  };
 
-  changeColorHandler = (color) => {
+  changeColorHandler = color => {
     this.setState({
-      bgColor: color.target.value,
-    })
-  }
+      bgColor: color.target.value
+    });
+  };
 
-  submitHandler = (event) => {
-    var body = document.querySelector('body');
+  submitHandler = event => {
+    var body = document.querySelector("body");
     body.style.background = this.state.bgColor;
     event.preventDefault();
+  };
 
-  }
-  
   render() {
     return (
       <div className="App">
-          <h1 id="header">Hello</h1>
-          <div className='choices'>
-
-          <ColorPickerInput initalbgColor={this.state.bgColor} bgColor={this.changeColorHandler} typedSubmit={this.submitHandler}/>
-          <ColorPickerList initialBGColor={this.state.bgColor} bgColor={this.changeColorHandler} listSubmit={this.submitHandler}/>
-          </div>
-     </div>
+        <h1 id="header">Hello</h1>
+        <div className="choices">
+          <ColorPickerInput
+            initalbgColor={this.state.bgColor}
+            bgColor={this.changeColorHandler}
+            typedSubmit={this.submitHandler}
+          />
+          <ColorPickerList
+            initialBGColor={this.state.bgColor}
+            bgColor={this.changeColorHandler}
+            listSubmit={this.submitHandler}
+          />
+        </div>
+      </div>
     );
   }
 }
